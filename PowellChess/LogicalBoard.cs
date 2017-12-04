d4cb75768c34c7d0127dfb124f032bcf5a7133cf
Isaac Powell <powelli91@outlook.com>
2017-12-03 21:11:51 -0800

    added structure for logical represenation of board and piece drawing

---
+++ PowellChess/LogicalBoard.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowellChess
+{
+    /// <summary>
+    /// Used to represent the logic and enforce
+    /// rules of a chess board.
+    /// </summary>
+    class LogicalBoard
+    {
+        /// <summary>
+        /// represents the pieces on the board
+        /// the key for int values to piece is as follows:
+        /// 0 => blank square
+        /// 2 => white king
+        /// -1 => outside of board, used to make move validation simpler
+        /// the 21st element is A1
+        /// </summary>
+        private int[] board;
+
+        private int[] boardKey = new int[64] {
+            21, 22, 23, 24, 25, 26, 27, 28,
+            31, 32, 33, 34, 35, 36, 37, 38,
+            41, 42, 43, 44, 45, 46, 47, 48,
+            51, 52, 53, 54, 55, 56, 57, 58,
+            61, 62, 63, 64, 65, 66, 67, 68,
+            71, 72, 73, 74, 75, 76, 77, 78,
+            81, 82, 83, 84, 85, 86, 87, 88,
+            91, 92, 93, 94, 95, 96, 97, 98
+        };
+
+        /// <summary>
+        /// constructor for logical board returns self
+        /// with a fresh board setup and white to move
+        /// </summary>
+        public LogicalBoard()
+        {
+            board = new int[120] {
+                -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+                -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+                -1, 0, 0, 0, 0, 2, 0, 0, 0, -1,
+                -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+                -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+                -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+                -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+                -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+                -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
+                -1, 0, 0, 0, 0, 2, 0, 0, 0, -1,
+                -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+                -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
+            };
+        }
+        
+        public int[] RetrieveBoardState()
+        {
+            int[] ret = new int[64];
+
+            for (int i = 0; i < 64; i++)
+            {
+                ret[i] = board[boardKey[i]];
+            }
+
+            return ret;
+        }
+        
+        public void DiscoverMoves()
+        {
+            return;
+        }
+    }
+}
--- PowellChess/Main.cs
+++ PowellChess/Main.cs
@@ -2,6 +2,9 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+using System.Collections.Generic;
+using System;
+
 namespace PowellChess
 {
     /// <summary>
@@ -12,9 +15,19 @@ namespace PowellChess
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        LogicalBoard logicalBoard;
+
+        // board display constants
+        const int squareSize = 72;
+        const int edgePadding = 72;
+
         // sprite variables for squares and pieces
         private Texture2D lightSquare;
         private Texture2D darkSquare;
+
+        private Texture2D whiteKing;
+
+        private Dictionary<int, Texture2D> pieceSprites;
 
         public Main()
         {
@@ -39,6 +52,9 @@ namespace PowellChess
 
             // make standard mouse cursor visible
             this.IsMouseVisible = true;
+
+            // creat logical board
+            logicalBoard = new LogicalBoard();
         }
 
         /// <summary>
@@ -49,10 +65,12 @@ namespace PowellChess
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            pieceSprites = new Dictionary<int, Texture2D>();
 
-            // sprites for board squares
+            // sprites for board squares and pieces
             lightSquare = this.Content.Load<Texture2D>("light_square");
             darkSquare = this.Content.Load<Texture2D>("dark_square");
+            pieceSprites[2] = this.Content.Load<Texture2D>("white_king");
         }
 
         /// <summary>
@@ -85,21 +103,45 @@ namespace PowellChess
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
+            // TODO verify length of boardstate
+            int[] boardState = logicalBoard.RetrieveBoardState();
             GraphicsDevice.Clear(Color.LightGray);
 
             spriteBatch.Begin();
 
             // draw board squares
-            for (int x = 0; x < 8; x++)
+            int xPos;
+            int yPos;
+            int boardIndex = 0;
+
+            // spans across one row at a time starting with 8th row
+            for (int y = 0; y < 8; y++)
             {
-                for (int y = 0; y < 8; y++)
+                for (int x = 0; x < 8; x++)
                 {
+
+                    xPos = x * squareSize + edgePadding;
+                    yPos = y * squareSize + edgePadding;
+
                     if ((x + y) % 2 == 0)
                     {
-                        spriteBatch.Draw(lightSquare, new Vector2(x*72 + 72, y*72 + 72), Color.White);
+                        spriteBatch.Draw(lightSquare, new Vector2(xPos, yPos), Color.White);
                     } else {
-                        spriteBatch.Draw(darkSquare, new Vector2(x*72 + 72, y*72 + 72), Color.White);
+                        spriteBatch.Draw(darkSquare, new Vector2(xPos, yPos), Color.White);
                     }
+
+                    //TODO ensure no negatives
+                    // TDOO ensure board index is not negative
+                    if (boardState[boardIndex] != 0)
+                    {
+
+                        //TODO verify that x + y is in keys
+                        spriteBatch.Draw(pieceSprites[boardState[boardIndex]], new Vector2(xPos, yPos), Color.White);
+                    }
+                    /*else if (boardState[x + y] == -1) {
+                        Console.WriteLine()
+                    }*/
+                    boardIndex++;
                 }
             }
 
